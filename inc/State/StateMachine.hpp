31319535ef0337f988b93aa72d8fb642efeb9763
Hossam <7osssam@protonmail.ch>
2024-05-25 00:45:53 +0300

    Refactor state machine classes and add documentation

--- inc/State/StateMachine.hpp
+++ inc/State/StateMachine.hpp
@@ -10,7 +10,9 @@
 #include "QuoteManager.hpp"
 #include "WordDetector.hpp"
 
-// Abstract base class for states
+/**
+ * @brief The base class for all states in the state machine.
+ */
 class State
 {
 public:
@@ -18,7 +20,12 @@ public:
 	virtual void execute() = 0;
 };
 
-// State machine class to manage states
+/**
+ * @brief Represents a state machine that manages different states.
+ *
+ * The StateMachine class is responsible for managing different states and transitioning between them.
+ * It provides functionality to set the current state, execute the current state, and drive the state machine.
+ */
 class StateMachine
 {
 private:
@@ -28,11 +35,11 @@ public:
 	// Enum for representing different states
 	enum class StateType
 	{
-		GetQuote,
-		Exit,
-		GetHistory,
-		Backup,
-		Restore
+		GetQuote,	/**< The state for getting a quote based on the user's emotion. */
+		Exit,		/**< The state for exiting the program. */
+		GetHistory, /**< The state for getting the history of quotes. */
+		Backup,		/**< The state for creating a backup of the quote history. */
+		Restore		/**< The state for restoring the quote history from a backup. */
 	};
 
 	/**
@@ -60,22 +67,26 @@ public:
 	void run();
 };
 
-// Concrete state classes
+/**
+ * @class GetQuoteState
+ * @brief Represents a state in the state machine that retrieves a random quote based on the user's current emotion.
+ * 
+ * This state is responsible for prompting the user for their current emotion, detecting the emotion based on the input,
+ */
 class GetQuoteState : public State
 {
 private:
-	std::string	  line{};
-	std::string	  quote{};
-	std::string	  emotion{};
-	QuoteManager& quoteManager_;
-	WordDetector& wordDetector_;
+	std::string	  line{};		 /**< The current line of text. */
+	std::string	  quote{};		 /**< The random quote. */
+	std::string	  emotion{};	 /**< The user's current emotion. */
+	QuoteManager& quoteManager_; /**< The singleton instance of the QuoteManager class. */
+	WordDetector& wordDetector_; /**< The singleton instance of the WordDetector class. */
 
 public:
 	/**
 	 * @brief Constructs a new instance of the GetQuoteState class.
 	 * 
 	 * This constructor initializes the GetQuoteState object by obtaining the singleton instances of the QuoteManager and WordDetector classes.
-	 * 
 	 */
 	GetQuoteState();
 
@@ -88,10 +99,16 @@ public:
 	void execute() override;
 };
 
+/**
+ * @class GetHistoryState
+ * @brief Represents the state for retrieving and printing the history quotes.
+ * 
+ * This state is responsible for printing the history quotes stored in the quote manager.
+ */
 class GetHistoryState : public State
 {
 private:
-	QuoteManager& quoteManager_;
+	QuoteManager& quoteManager_; /**< The singleton instance of the QuoteManager class. */
 
 public:
 	/**
@@ -109,10 +126,16 @@ public:
 	void execute() override;
 };
 
+/**
+ * @class BackupState
+ * @brief Represents the state for creating a backup of the current state of the quote manager.
+ * 
+ * This state is responsible for creating a backup of the current state of the quote manager.
+ */
 class BackupState : public State
 {
 private:
-	Caretaker caretaker;
+	Caretaker caretaker; /**< The caretaker object used to manage the memento objects. */
 
 public:
 	/**
@@ -126,10 +149,17 @@ public:
 	void execute() override;
 };
 
+/**
+ * @class RestoreState
+ * @brief Represents the state for restoring the state of the quote manager.
+ * 
+ * This state is responsible for restoring the state of the quote manager from a backup.
+ * @embed{diagram_hierarchy_class.svg}
+ */
 class RestoreState : public State
 {
 private:
-	Caretaker caretaker;
+	Caretaker caretaker; /**< The caretaker object used to manage the memento objects. */
 
 public:
 	/**
@@ -141,6 +171,12 @@ public:
 	void execute() override;
 };
 
+/**
+ * @class ExitState
+ * @brief Represents the exit state of the state machine.
+ * 
+ * The ExitState class is a derived class of the State class. It is responsible for terminating the program with a successful exit status.
+ */
 class ExitState : public State
 {
 public:
