85849b2d3b38a4e5cb6170c337c81dbbe6998ba9
Hossam <7osssam@protonmail.ch>
2024-05-25 00:46:48 +0300

    Add Doxgen documentation

--- inc/UserSpace/PasswordHandler.hpp
+++ inc/UserSpace/PasswordHandler.hpp
@@ -30,6 +30,13 @@ public:
 	virtual bool handleRequest(const std::string& str) = 0;
 };
 
+/**
+ * @class PasswordLengthHandler
+ * @brief A handler class that checks the length of a password.
+ * 
+ * The PasswordLengthHandler class is responsible for handling requests to check the length of a password.
+ * It inherits from the Handler base class and implements the handleRequest method to perform the length check.
+ */
 class PasswordLengthHandler : public Handler
 {
 private:
@@ -37,14 +44,19 @@ private:
 
 public:
 	/**
-	 * Sets the successor handler for the PasswordLengthHandler.
+	 * @brief Sets the successor handler for the PasswordLengthHandler.
+	 * 
+	 * This method sets the next handler to be used as the successor for the PasswordLengthHandler.
 	 * 
 	 * @param next The next handler to be set as the successor.
 	 */
 	void setSuccessor(std::shared_ptr<Handler> next) override;
 
 	/**
-	 * Handles the request to check the length of a password.
+	 * @brief Handles the request to check the length of a password.
+	 * 
+	 * This method handles the request to check the length of a password string.
+	 * It checks if the password length is at least 8 characters long and returns true if it is, false otherwise.
 	 * 
 	 * @param str The password string to be checked.
 	 * @return True if the password length is at least 8 characters long, false otherwise.
@@ -52,6 +64,13 @@ public:
 	bool handleRequest(const std::string& str) override;
 };
 
+/**
+ * @class PasswordDigitHandler
+ * @brief A handler class that checks if a password contains at least one digit.
+ * 
+ * The PasswordDigitHandler class is responsible for handling requests to check if a password contains at least one digit.
+ * It inherits from the Handler base class and implements the handleRequest method to perform the digit check.
+ */
 class PasswordDigitHandler : public Handler
 {
 private:
@@ -74,6 +93,13 @@ public:
 	bool handleRequest(const std::string& str) override;
 };
 
+/**
+ * @class PasswordSpecialCharHandler
+ * @brief A handler class that checks if a password contains at least one special character.
+ * 
+ * The PasswordSpecialCharHandler class is responsible for handling requests to check if a password contains at least one special character.
+ * It inherits from the Handler base class and implements the handleRequest method to perform the special character check.
+ */
 class PasswordSpecialCharHandler : public Handler
 {
 private:
