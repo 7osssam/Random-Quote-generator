4ccb600909ec668c05b8d132950c94945334e8c6
Hossam <7osssam@protonmail.ch>
2024-05-24 03:17:57 +0300

    Refactor QuoteManager class and add QuoteManager.cpp

--- inc/QuoteManager.hpp
+++ inc/QuoteManager.hpp
@@ -1,119 +1,31 @@
 #ifndef __QUOTEMANAGER_H__
 #define __QUOTEMANAGER_H__
 
+#include "FilesManager.hpp"
 
-#include "CSVManager.hpp"
 #include <algorithm>
 #include <string>
 #include <vector>
 
+#define QUOTES_FILE "../data/Quotes.csv"
+
 class QuoteManager
 {
 private:
-    CSVManager &csvManager_;
-    std::vector<std::string> history_;
-    QuoteManager(CSVManager &csvManager) : csvManager_(csvManager)
-    {
-    }
+	FilesManager			 filesManager_;
+	std::vector<std::string> history_;
+	QuoteManager();
 
-    QuoteManager(const QuoteManager &) = delete;
-    QuoteManager &operator=(const QuoteManager &) = delete;
-
+	QuoteManager(const QuoteManager&) = delete;
+	QuoteManager& operator=(const QuoteManager&) = delete;
 
 public:
-    // create singleton
-    static QuoteManager &getInstance(CSVManager &csvManager)
-    {
-        static QuoteManager instance(csvManager);
-        return instance;
-    }
+	// create singleton
+	static QuoteManager& getInstance();
+	std::string			 getRandomQuote(const std::string& emotion);
 
-
-    std::string getRandomQuote(const std::string &filename, const std::string &emotion)
-    {
-        auto quotes = csvManager_.getData(filename, emotion);
-        if (!quotes.empty())
-        {
-            int index = rand() % quotes.size();
-
-            // Add the quote to the history
-            history_.push_back(quotes[index]);
-
-            return quotes[index];
-        }
-        return "No quotes available for this emotion.";
-    }
-
-    // get the history
-    std::vector<std::string> getHistory() const
-    {
-        return history_;
-    }
-
-    // set the history
-    void setHistory(const std::vector<std::string> &history)
-    {
-        history_ = history;
-    }
-};
-
-class WordDetector
-{
-private:
-    CSVManager &csvManager_;
-    WordDetector(CSVManager &csvManager) : csvManager_(csvManager)
-    {
-    }
-
-    WordDetector(const WordDetector &) = delete;
-    WordDetector &operator=(const WordDetector &) = delete;
-
-    std::vector<std::string> split(const std::string &s, char delimiter)
-    {
-        std::vector<std::string> words;
-        std::stringstream ss(s);
-        std::string word;
-
-        while (std::getline(ss, word, delimiter))
-        {
-            // Skip empty words
-            if (!word.empty())
-            {
-                words.push_back(word);
-            }
-        }
-
-        return words;
-    }
-
-public:
-    // create singleton
-    static WordDetector &getInstance(CSVManager &csvManager)
-    {
-        static WordDetector instance(csvManager);
-        return instance;
-    }
-
-
-    bool containsWord(const std::string &filename, const std::string &emotion, const std::string &line)
-    {
-        bool result = false;
-        auto words = csvManager_.getData(filename, emotion);
-
-        //split the line into words
-        std::vector<std::string> lineWords = split(line, ' ');
-
-        // search in a line a word from the words vector
-        for (const auto &word : words)
-        {
-            if (std::find(lineWords.begin(), lineWords.end(), word) != lineWords.end())
-            {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
+	std::vector<std::string> getHistory() const;
+	void					 setHistory(const std::vector<std::string>& history);
 };
 
 #endif // __QUOTEMANAGER_H__
---
+++ src/QuoteManager.cpp
@@ -0,0 +1,38 @@
+#include "../inc/QuoteManager.hpp"
+
+QuoteManager::QuoteManager() : filesManager_{FileManagerFactory::FileType::CSV}
+{
+	filesManager_.loadFile(QUOTES_FILE);
+}
+
+QuoteManager& QuoteManager::getInstance()
+{
+	static QuoteManager instance;
+	return instance;
+}
+
+std::string QuoteManager::getRandomQuote(const std::string& emotion)
+{
+	//auto quotes = csvManager_.getData(filename, emotion);
+	auto quotes = filesManager_.getData(QUOTES_FILE, emotion);
+	if (!quotes.empty())
+	{
+		int index = rand() % quotes.size();
+
+		// Add the quote to the history
+		history_.push_back(quotes[index]);
+
+		return quotes[index];
+	}
+	return "No quotes available for this emotion.";
+}
+
+std::vector<std::string> QuoteManager::getHistory() const
+{
+	return history_;
+}
+
+void QuoteManager::setHistory(const std::vector<std::string>& history)
+{
+	history_ = history;
+}
